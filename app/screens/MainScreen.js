c14cbd2f12c2b0f5e4e292e1d6566a922d20d072
Hunkyo Jung <hunkyo.jung@gmail.com>
2017-08-27 00:25:52 +0900

    Add vector icons

--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -1,17 +1,67 @@
 /*
  * @flow
  */
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
+import { List, ListItem, Button } from 'react-native-elements';
 
-class MainScreen extends React.Component {
+const { width: deviceWidth, height: deviceHeight } = Dimensions.get('window');
+
+const ASPECT_RATIO = deviceWidth / deviceHeight;
+const LATITUDE = 37.400373;
+const LONGITUDE = 127.104913;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+class MainScreen extends Component {
+  static navigationOptions = () => ({
+    title: 'SuperSafe',
+  });
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>MainScreen</Text>
+        <MapView
+          provider={this.props.provider}
+          style={styles.map}
+          scrollEnabled={true}
+          zoomEnabled={true}
+          pitchEnabled={true}
+          rotateEnabled={false}
+          initialRegion={this.state.region}
+        >
+          <MapView.Marker
+            title="This is a title"
+            description="This is a description"
+            coordinate={this.state.region}
+          />
+        </MapView>
+
+        <List containerStyle={{ marginTop: 0 }}>
+          <ListItem title={'차량 정보'} />
+          <ListItem title={'카드 정보'} />
+        </List>
+
         <Button
-          title={'Go to next'}
-          onPress={() => this.props.navigation.navigate('Map')}
+          buttonStyle={{ marginRight: 0, marginLeft: 0 }}
+          disabled
+          raised
+          large
+          title={'요청하기'}
         />
       </View>
     );
@@ -21,8 +71,10 @@ class MainScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: deviceWidth,
+    height: 250,
   },
 });
 
